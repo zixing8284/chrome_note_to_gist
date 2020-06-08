52e6d498d42b83292bd358ed0b97d338945195b7
sasasa10 <zixing8284@gmail.com>
2020-06-08 23:31:52 +0800

    fuck myself

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 class Github {
     // 整个index.html string
     notesource = document.documentElement.outerHTML;
@@ -12,8 +14,9 @@ class Github {
     });
     // access_token
     token = "";
-    constructor() {
-        this.requestOptions = {
+    // gist_id
+    gist_id = "";
+        requestOptions = {
             method: 'POST',
             headers: {
                 "Authorization": "token " + this.token,
@@ -22,10 +25,22 @@ class Github {
             body: this.raw,
             redirect: 'follow'
         };
-    }
-    // 上传至gist
+    // 新建,上传至gist
     fetch_upload() {
         fetch("https://api.github.com/gists", this.requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    }
+    // 更新gist
+    fetch_update() {
+        fetch(`https://api.github.com/gists/${gist_id}`,{method: 'PATCH',
+        headers: {
+            "Authorization": "token " + this.token,
+            "Content-Type": "application/json"
+        },
+        body: this.raw,
+        redirect: 'follow'})
         .then(response => response.text())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
@@ -39,9 +54,21 @@ const app = new Vue({
         list1:[1,2,3,4,5]
     },
     methods: {
+        createData() {
+            let github = new Github();
+            github.fetch_upload();
+        },
         updateData() {
             let github = new Github();
             github.fetch_upload();
+        },
+        handleInput($event) {
+            this.list1[index] = $event.target.innerText;
+            console.log(this.list1);
+        },
+        addNewList($event) {
+            this.list1.push($event.target.value)
+            console.log(this.list1)
         }
     }
 })
